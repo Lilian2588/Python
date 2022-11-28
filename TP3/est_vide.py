9106e7578d0770c89900f397fef90af1464eb84f
Lilian2588 <lilian.saglibene1@gmail.com>
2022-11-28 15:21:06 +0100

    fonctions est_vide et remplacer une lettre

---
+++ TP3/est_vide.py
@@ -0,0 +1,15 @@
+"""
+    TP3
+    réalisé le 28/11/2022
+    Par Saglibene Lilian et Glemet Augustin 
+    Objectif : Créer une fonction qui renvoie si une liste est vide ou non
+
+
+"""
+
+def est_vide(L) : 
+    if len(L)==0 : 
+        return False 
+    else : 
+        return True
+    
---
+++ TP3/replace_blank.py
@@ -0,0 +1,14 @@
+"""
+    TP3
+    réalisé le 28/11/2022
+    Par Saglibene Lilian et Glemet Augustin 
+    Objectif : Créer une fonction pour remplacer les underscore par la lettre
+
+"""
+def replace_blank(underscore, lettre_prop) :
+    if not(est_vide(verif_lettre)) : 
+        for i in range(verif_lettre) : 
+            underscore[i] == lettre_prop
+    else :
+        return
+    return underscore
