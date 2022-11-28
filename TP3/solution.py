a89f7d2c5500ed6dd2dd0ba2f17f129a9f1de87c
Augustin Glemet <augustin.glemet-gabazzi@cpe.fr>
2022-11-28 15:01:23 +0100

    bonne habitudes

--- TP3/solution.py
+++ TP3/solution.py
@@ -6,7 +6,13 @@ fichier contenant une liste de mots que l'on appelera dans notre jeu du pendu
 """
 
 from random import randint
+
 def trie(solution):
+    """
+    fontion qui permet de classés les mots d'une liste par taille puis par ordre alphabétique
+    entrée: liste de mot
+    sortie: liste de mot triée
+    """
     solution_alph = sorted(solution)
     solution_len = sorted(solution_alph,key=len)
   
@@ -16,8 +22,13 @@ def trie(solution):
 lst=["chat","souris","anticonstitutionnellement","interface","composants","application","carré"]
 
 def mot_au_hasard():
-    mot=trie(lst)
-    i=randint(0,len(mot)-1)
+    """
+    fonction qui permet de renvoyer un mot de notre liste au hasard
+    entrée= none
+    sortie: liste de caractère
+    """
+    mot = trie(lst)
+    i = randint(0,len(mot)-1)
     mot_au_hasard = mot[i]
     return(mot_au_hasard)
     
